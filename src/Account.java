a61ad2491df198eef93a0065a21432d17e6e83cf
Oliver Chang <ochang@xpanxion.com>
2022-06-28 18:25:30 -0400

    Account problem completed

---
+++ src/Account.java
@@ -0,0 +1,53 @@
+public class Account {
+    private String id;
+    private String name;
+    private double balance;
+
+    public Account(String id, String name){
+        this.id = id;
+        this.name = name;
+    }
+    public Account(String id, String name, double balance){
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+    public String getId(){
+        return this.id;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public double getBalance(){
+        return this.balance;
+    }
+    public void credit(double amount){
+        this.balance += amount;
+    }
+    public boolean debit(double amount){
+
+        if (this.balance - amount <= 0){
+            System.out.println("Amount exceeded");
+            return false;
+        }
+        else
+        {
+            this.balance -= amount;
+            return true;
+        }
+        // returns false if no balance, "amount exceeded"
+    }
+    public void transferTo(Account another, double amount){
+        boolean fundsAvailable = debit(amount);
+        if (fundsAvailable) {
+            // this.balance is already subtracted in debit()
+            another.balance += amount;
+            System.out.println("Transfer completed.");
+        }
+    }
+    public String accountDetails(){
+        String details = String.format("%s, the current balance in your account %s is $%f", this.name, this.id, this.balance);
+        return details;
+    }
+
+}
